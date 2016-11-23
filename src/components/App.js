3bd38d9c6db24e2432f313ab8765b7c267d859f4
Estella Gonzalez Madison <emadison@blurb.com>
2016-11-23 12:21:46 -0700

    Move Buildkit assets to index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../img/logo.svg';
 import '../css/App.css';
-import '../../node_modules/blurb-buildkit/assets/css/main-drupal.css';
-import Modernizr from '../../node_modules/blurb-buildkit/assets/js/vendor/modernizr/modernizr-custom.min.js';
-import Buildkit from '../../node_modules/blurb-buildkit/assets/js/main-eng.js';
 
 class App extends Component {
   render() {
@@ -14,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/compontents/App.js</code> and save to reload.
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './css/index.css';
+import '../node_modules/blurb-buildkit/assets/css/main-drupal.css';
+import Modernizr from '../node_modules/blurb-buildkit/assets/js/vendor/modernizr/modernizr-custom.min.js';
+import Buildkit from '../node_modules/blurb-buildkit/assets/js/main-eng.js';
 
 ReactDOM.render(
   <App />,
