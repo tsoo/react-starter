a198f4aaf35ce730abb31406a6084ed0507ad288
Estella Gonzalez Madison <emadison@blurb.com>
2016-12-05 14:51:19 -0700

    Fix header & footer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Header from './components/Header';
 import './css/index.css';
 import '../node_modules/blurb-buildkit/assets/css/main-drupal.css';
 // eslint-disable-next-line
@@ -9,6 +10,11 @@ import Modernizr from '../node_modules/blurb-buildkit/assets/js/vendor/modernizr
 import Buildkit from '../node_modules/blurb-buildkit/assets/js/mainReact.js';
 
 ReactDOM.render(
+  <Header />,
+  document.getElementById('header')
+);
+
+ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('main-content')
 );
