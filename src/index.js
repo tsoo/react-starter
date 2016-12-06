e7b12138ca71d1f96f510d5f49ee7211c4787d7b
Estella Gonzalez Madison <emadison@blurb.com>
2016-12-06 11:52:26 -0700

    Use bower for Buildkit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import Header from './components/Header';
 import './css/index.css';
-import '../node_modules/blurb-buildkit/assets/css/main-drupal.css';
-// eslint-disable-next-line
-import Modernizr from '../node_modules/blurb-buildkit/assets/js/vendor/modernizr/modernizr-custom.min.js';
-// eslint-disable-next-line
-import Buildkit from '../node_modules/blurb-buildkit/assets/js/mainReact.js';
 
 ReactDOM.render(
   <Header />,
